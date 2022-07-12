c05886de13b649f71c121fc10c20d93712ff8d17
edwardf32 <76261985+edwardf32@users.noreply.github.com>
2022-07-12 15:49:49 +0100

    Update streamlit_app.py
    fixing type

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # formats the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
